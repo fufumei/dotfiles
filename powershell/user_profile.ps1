fe25ad0d8829a4870af0715074b368001e3c52a5
fumei <robin.fumei@gmail.com>
2023-03-11 16:19:05 +0100

    initial commit

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,19 @@
+# Prompt
+oh-my-posh init pwsh --config 'C:\Users\robin\.config\powershell\fumei.omp.json' | Invoke-Expression
+
+# Alias
+Set-Alias vsc code
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias c clear
+Set-Alias grep findstr
+
+function Shortcut-repo () {
+    cd D:/repos
+}
+Set-Alias repo Shortcut-repo
+
+function take($1) {
+    mkdir -p $1
+    cd $1
+}
