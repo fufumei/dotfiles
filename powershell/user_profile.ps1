ddfc035881e533cbba90af748f1540a9c4333dfa
fumei <robin.fumei@gmail.com>
2024-09-20 14:51:44 +0200

    Update aliases

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,5 +1,5 @@
 # Prompt
-oh-my-posh init pwsh --config 'C:\Users\robin\.config\powershell\fumei.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config '~\.config\powershell\fumei.omp.json' | Invoke-Expression
 
 # Alias
 Set-Alias vsc code
@@ -7,13 +7,14 @@ Set-Alias ll ls
 Set-Alias g git
 Set-Alias c clear
 Set-Alias grep findstr
+Set-Alias repo repoShortcut
+New-Alias pity ~\.config\powershell\scripts\fgo-pity.exe
 
-function Shortcut-repo () {
-    cd D:/repos
+function repoShortcut () {
+    Set-Location D:/repos
 }
-Set-Alias repo Shortcut-repo
 
-function take($1) {
-    mkdir -p $1
-    cd $1
+function startShizuki() {
+    & "D:\Program Files\Netease\MuMuPlayerGlobal-12.0\shell\adb.exe" connect 127.0.0.1:7555
+    & "D:\Program Files\Netease\MuMuPlayerGlobal-12.0\shell\adb.exe" shell sh /storage/emulated/0/Android/data/moe.shizuku.privileged.api/start.sh
 }
